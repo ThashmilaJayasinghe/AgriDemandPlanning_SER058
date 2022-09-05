967e3d8a31ab8c45b815b8bb344e688081d7f021
Thashmila Jayasinghe <thashmilajayasinghe@gmail.com>
2022-09-05 11:48:05 +0530

    AllFarmers page created with table

--- frontend/src/components/layouts/Admin.jsx
+++ frontend/src/components/layouts/Admin.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import ThemeAction from "../../redux/actions/ThemeAction";
 
+import Sidebar from "../Sidebar"
 import Dashboard from "../../pages/AdminDash";
+import AllFarmers from "../../pages/AllFarmers";
 
 const Layout = () => {
   return (
     <>
+      {/*<Sidebar />*/}
       <h1>Admin Pages</h1>
       <Routes>
         <Route path="/dash" element={<Dashboard />} />
+        <Route path="/farmers" element={<AllFarmers />} />
       </Routes>
     </>
   );
