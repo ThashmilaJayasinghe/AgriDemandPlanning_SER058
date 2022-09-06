a03ed42fccfe226a1273bb579613f6692556d73d
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-09-07 03:11:02 +0530

    prettified changes

--- backend/dal/seller/demand.model.js
+++ backend/dal/seller/demand.model.js
@@ -1,32 +1,33 @@
 const mongoose = require("mongoose");
 
 const demandSchema = mongoose.Schema({
-  buyerID:{
-    type: String
+  buyerID: {
+    type: String,
   },
-  demands:[{
-    category: {
-      type: String,
-      require: [true, "Please select category"],
+  demands: [
+    {
+      category: {
+        type: String,
+        require: [true, "Please select category"],
+      },
+      type: {
+        type: String,
+        require: [true, "Please select type"],
+      },
+      sellings: {
+        type: Number,
+        require: [true, "Please add sellings for a year"],
+      },
+      unitPrice: {
+        type: Number,
+        require: [true, "Please enter unit price"],
+      },
+      remarks: {
+        type: String,
+        require: [false, "Please add remarks if any"],
+      },
     },
-    type: {
-      type: String,
-      require: [true, "Please select type"],
-    },
-    sellings: {
-      type: Number,
-      require: [true, "Please add sellings for a year"],
-    },
-    unitPrice: {
-      type: Number,
-      require: [true, "Please enter unit price"],
-    },
-    remarks: {
-      type: String,
-      require: [false, "Please add remarks if any"],
-    },
-  }]
-
+  ],
 });
 
 module.exports = mongoose.model("Demand", demandSchema);
