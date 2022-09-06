450e707266c0d77e67f045d7a31041bad81b070f
it20114312 <it20114312@my.sliit.lk>
2022-09-06 14:01:02 +0530

    buyer added.

---
+++ backend/dal/buyer/buyer.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const buyerSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, 'please enter name']
+    },
+    NIC: {
+        type: String,
+        required: [true, 'please enter NIC']
+    },
+    ShopName: {
+        type: String,
+        required: [true, 'please enter shop name' ]
+    },
+    gender:{
+        type: String,
+        required: [true, 'please enter gender']
+    },
+    address: {
+        type: String,
+        required: [true, 'please enter address']
+    },
+    province: {
+        type: String,
+        required: [true, 'please enter province']
+    },
+    district: {
+        type: String,
+        required: [true, 'please enter district']
+    },
+    email: {
+        type: String,
+        required: [true, 'please enter email']
+    },
+    contactNumber:{
+        type: String,
+        required: [true, 'please enter contact number']
+    },
+    // userName: {
+    //     type: String,
+    //     required: [true, 'please enter user name ']
+    // },
+    // password: {
+    //     type: String,
+    //     required: [true, 'please enter password']
+    // },
+});
+
+module.exports = mongoose.model('Buyer', buyerSchema);
