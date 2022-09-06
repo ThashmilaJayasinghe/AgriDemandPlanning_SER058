67d6be1fca29fc5e00c3c3c4cae11d992047e5f0
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-09-06 15:19:52 +0530

    demand form added

---
+++ frontend/src/pages/buyer/AddDemand.jsx
@@ -0,0 +1,213 @@
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import React, { Fragment, useState } from "react";
+import FormWrapper from "../../components/wrappers/FormWrapper";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const AddDemand = () => {
+  const [categories, setCategories] = useState([]);
+  const [types, setTypes] = useState();
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedType, setSelectedType] = useState("");
+  const [sellings, setSellings] = useState(0.0);
+  const [unitPrice, setUnitPrice] = useState(0.0);
+  const [remarks, setRemarks] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Submitted");
+    console.log(selectedCategory, selectedType, sellings, unitPrice, remarks);
+  };
+
+  return (
+    <FormWrapper>
+      <>
+        <div className="my-16">
+          <p className="font-semibold text-2xl text-center">
+            Add demand for a product
+          </p>
+
+          <label
+            htmlFor="category"
+            className="block text-base font-medium text-gray-700 mt-6"
+          >
+            Category :
+          </label>
+
+          {/* dropdown start */}
+          <Menu
+            as="div"
+            className="relative inline-block text-left w-full mt-2"
+          >
+            <div>
+              <Menu.Button className="inline-flex py-2 px-5 border border-gray-300 md:mr-0 md:pr-0 w-full rounded-md text-sm font-medium text-gray-700 active:ring-2 active:ring-emerald-400 active:border-0 focus:ring-2 focus:ring-emerald-400 focus:border-0">
+                <div className="text-gray-500 font-normal">
+                  {selectedCategory.title}
+                </div>
+                <ChevronDownIcon
+                  color="#a3a3a3"
+                  className=" ml-0.5 h-5 w-3 absolute right-6"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-emerald-400 ring-opacity-5 focus:outline-none overflow-visible z-50">
+                <div className="py-1">
+                  {categories.map((item, idx) => {
+                    return (
+                      <Menu.Item key={item.id}>
+                        {({ active }) => (
+                          <a
+                            onClick={() => setSelectedCategory(item)}
+                            className={classNames(
+                              active
+                                ? "bg-emerald-50 text-gray-700"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            {item.title}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    );
+                  })}
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+          {/* dropdown end */}
+
+          <label
+            htmlFor="type"
+            className="block text-base font-medium text-gray-700 mt-6"
+          >
+            Type :
+          </label>
+
+          <Menu
+            as="div"
+            className="relative inline-block text-left w-full mt-2"
+          >
+            <div>
+              <Menu.Button className="inline-flex py-2 px-5 border border-gray-300 md:mr-0 md:pr-0 w-full rounded-md text-sm font-medium text-gray-700 active:ring-2 active:ring-emerald-400 active:border-0 focus:ring-2 focus:ring-emerald-400 focus:border-0">
+                <div className="text-gray-500 font-normal">{selectedType}</div>
+                <ChevronDownIcon
+                  color="#a3a3a3"
+                  className=" ml-0.5 h-5 w-3 absolute right-6"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-emerald-400 ring-opacity-5 focus:outline-none overflow-visible z-50">
+                <div className="py-1">
+                  {/* {selectedCategory.types.map((item, idx) => {
+                    return (
+                      <Menu.Item key={item}>
+                        {({ active }) => (
+                          <a
+                            onClick={() => setSelectedType(item)}
+                            className={classNames(
+                              active
+                                ? "bg-emerald-50 text-gray-700"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            {item}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    );
+                  })} */}
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+
+          <label
+            htmlFor="sellings"
+            className="block text-base font-medium text-gray-700 mt-6"
+          >
+            Selling in Kilograms (for 1 year) :
+          </label>
+          <input
+            type="number"
+            name="sellings"
+            id="sellings"
+            autoComplete="given-name"
+            className="mt-2 focus:ring-1 focus:ring-emerald-400 focus:border-emerald-400 block w-full shadow-sm sm:text-sm text-gray-600 border-gray-300 rounded-md"
+            onChange={(event) => {
+              setSellings(event.target.value);
+            }}
+          />
+          <label
+            htmlFor="price"
+            className="block text-base font-medium text-gray-700 mt-6"
+          >
+            Unit price in Rupees (1Kg) :
+          </label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            autoComplete="given-name"
+            className="mt-2 focus:ring-1 focus:ring-emerald-400 focus:border-emerald-400 block w-full shadow-sm sm:text-sm text-gray-600 border-gray-300 rounded-md"
+            onChange={(event) => {
+              setUnitPrice(event.target.value);
+            }}
+          />
+          <label
+            htmlFor="remarks"
+            className="block text-base font-medium text-gray-700 mt-6"
+          >
+            Remarks :
+          </label>
+          <input
+            type="text"
+            name="remarks"
+            id="remarks"
+            autoComplete="given-name"
+            className="mt-2 focus:ring-1 focus:ring-emerald-400 focus:border-emerald-400 block w-full shadow-sm sm:text-sm text-gray-600 border-gray-300 rounded-md"
+            onChange={(event) => {
+              setRemarks(event.target.value);
+            }}
+          />
+
+          <div className="flex items-center justify-center mt-10">
+            <div
+              className="px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-emerald-500 hover:bg-emerald-600 transition-colors cursor-pointer"
+              onClick={handleSubmit}
+            >
+              Submit
+            </div>
+          </div>
+        </div>
+      </>
+    </FormWrapper>
+  );
+};
+
+export default AddDemand;
