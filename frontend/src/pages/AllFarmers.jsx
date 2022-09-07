f401b69958264cbbae1a55c798c9e901fbf3a52b
it20114312 <it20114312@my.sliit.lk>
2022-09-07 17:30:48 +0530

    All Farmer pages Modified.

--- frontend/src/pages/AllFarmers.jsx
+++ frontend/src/pages/AllFarmers.jsx
@@ -24,19 +24,14 @@ export default function AllFarmers() {
     return (
         <div className="px-4 sm:px-6 lg:px-8">
             <div className="sm:flex sm:items-center">
-                <div className="sm:flex-auto">
-                    <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-                    <p className="mt-2 text-sm text-gray-700">
-                        A list of all the users in your account including their name, title, email and role.
-                    </p>
-                </div>
-                <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                    <button
-                        type="button"
-                        className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                    >
-                        Add user
-                    </button>
+
+                <div className="sm:flex sm:items-center">
+                    <div className="mt-4 sm:mt-0 sm:flex-none">
+                        <Link to="/admin/farmers/add">
+                            <button type="button" className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 sm:w-auto bg-green-600">
+                                +Add new
+                            </button></Link>
+                    </div>
                 </div>
             </div>
             <div className="mt-8 flex flex-col">
