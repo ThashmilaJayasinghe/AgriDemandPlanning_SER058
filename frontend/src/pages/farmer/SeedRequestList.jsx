4ea341451bf5182fae97f6a60facfdf03ce90ae1
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-09-07 23:17:23 +0530

    search filter added with some notifications

--- frontend/src/pages/farmer/SeedRequestList.jsx
+++ frontend/src/pages/farmer/SeedRequestList.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import {
-  CalendarIcon,
-  LocationMarkerIcon,
-  UsersIcon,
-} from "@heroicons/react/solid";
 import MyRequestsWrapper from "../../components/wrappers/farmer/MyRequestsWrapper";
 import { AiOutlineEdit } from "react-icons/ai";
 import { RiDeleteBin6Line } from "react-icons/ri";
@@ -13,60 +8,8 @@ import {
   viewFarmerSeedRequest,
 } from "../../api/SeedRequestAPI";
 import moment from "moment/moment";
-
-const positions = [
-  {
-    id: 1,
-    title: "Back End Developer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Engineering",
-    closeDate: "2020-01-07",
-    closeDateFull: "January 7, 2020",
-  },
-  {
-    id: 2,
-    title: "Front End Developer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Engineering",
-    closeDate: "2020-01-07",
-    closeDateFull: "January 7, 2020",
-  },
-  {
-    id: 3,
-    title: "User Interface Designer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Design",
-    closeDate: "2020-01-14",
-    closeDateFull: "January 14, 2020",
-  },
-];
-
-const staticRequests = [
-  {
-    id: "1462e85c-0cc9-4038-9f00-5aec06ea5880",
-    category: "Rice",
-    type: "Kalu Heenati",
-    Hectares: 1.41,
-    createdAt: "2021-07-05",
-  },
-  {
-    id: "b4abcf90-55f3-4df9-bb1a-2f457042102b",
-    category: "Rice",
-    type: "Keeri Samba",
-    Hectares: 3.0,
-    createdAt: "2021-07-25",
-  },
-  {
-    id: "d2c2d091-04e4-4e8d-9c3e-690bac268dfe",
-    category: "Rice",
-    type: "Kalu Heenati",
-    Hectares: 0.7,
-    createdAt: "2021-07-04",
-  },
-];
+import { FiAlertCircle } from "react-icons/fi";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const SeedRequestList = () => {
   const [myRequests, setMyRequests] = useState([]);
@@ -74,11 +17,19 @@ const SeedRequestList = () => {
   const [userId, setUserId] = useState("63177a60b7f1ef5842d83319");
   const [isDeleteSuccess, setIsDeleteSuccess] = useState(false);
   const [isSearchResultExists, setIsSearchResultExists] = useState(false);
+  const [filteredRequests, setFilteredRequests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function getRequests() {
-      await viewFarmerSeedRequest(userId, setMyRequests).then(() => {
+      await viewFarmerSeedRequest(
+        userId,
+        setMyRequests,
+        setFilteredRequests,
+        setIsSearchResultExists
+      ).then(() => {
         console.log("Data retrieved success");
+        setIsLoading(false);
       });
     }
     getRequests();
@@ -87,20 +38,46 @@ const SeedRequestList = () => {
   const onDelete = async (requestId) => {
     console.log("onDelete - ", requestId);
 
-    // use axios calls and delete from db
     await deleteSeedRequest(requestId, setIsDeleteSuccess).then(() => {
       console.log("Request deleted successfully");
     });
-
-    // get remaining data
-    await viewFarmerSeedRequest(userId, setMyRequests).then(() => {
+    await viewFarmerSeedRequest(
+      userId,
+      setMyRequests,
+      setFilteredRequests,
+      setIsSearchResultExists
+    ).then(() => {
       console.log("Data retrieved success");
     });
   };
 
+  // will develop in second sprint
   const onUpdate = (requestId) => {
     console.log("On update - ", requestId);
   };
+
+  useEffect(() => {
+    const filteredResult = myRequests.filter((request) => {
+      if (searchQuery === "") {
+        return request;
+      } else if (
+        request.category.toLowerCase().includes(searchQuery.toLowerCase())
+      ) {
+        return request;
+      } else if (
+        request.type.toLowerCase().includes(searchQuery.toLowerCase())
+      ) {
+        return request;
+      }
+    });
+
+    setFilteredRequests(filteredResult);
+    if (filteredResult.length > 0) {
+      setIsSearchResultExists(true);
+    } else {
+      setIsSearchResultExists(false);
+    }
+  }, [searchQuery]);
 
   return (
     <div>
@@ -124,98 +101,100 @@ const SeedRequestList = () => {
           />
         </div>
 
-        {myRequests
-          .filter((request) => {
-            if (searchQuery === "") {
-              return request;
-            } else if (
-              request.category.toLowerCase().includes(searchQuery.toLowerCase())
-            ) {
-              return request;
-            } else if (
-              request.type.toLowerCase().includes(searchQuery.toLowerCase())
-            ) {
-              return request;
-            } else if (
-              !request.type.toLowerCase().includes(searchQuery.toLowerCase())
-            ) {
-            }
-          })
-          .map((request, index) => (
-            <div
-              key={request._id}
-              className="bg-emerald-100 shadow overflow-hidden sm:rounded-md my-4"
-            >
-              <div className="block">
-                <div className="px-4 py-4 sm:px-6">
-                  <div className="mt-2 grid grid-cols-5">
-                    <p className="flex col-span-1 items-center text-sm text-gray-500">
-                      Category
-                    </p>
-                    <p className="mt-2 col-span-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                      : {request.category}
-                    </p>
+        {filteredRequests.map((request, index) => (
+          <div
+            key={request._id}
+            className="bg-emerald-100 shadow overflow-hidden sm:rounded-md my-4"
+          >
+            <div className="block">
+              <div className="px-4 py-4 sm:px-6">
+                <div className="mt-2 grid grid-cols-5">
+                  <p className="flex col-span-1 items-center text-sm text-gray-500">
+                    Category
+                  </p>
+                  <p className="mt-2 col-span-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                    : {request.category}
+                  </p>
+                </div>
+                <div className="mt-2 grid grid-cols-5 ">
+                  <p className="flex col-span-1 items-center text-sm text-gray-500 ">
+                    Type
+                  </p>
+                  <p className="mt-2 col-span-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                    : {request.type}
+                  </p>
+                </div>
+                <div className="mt-2 grid grid-cols-5">
+                  <p className="flex col-span-1 items-center text-sm text-gray-500">
+                    Hectares
+                  </p>
+                  <p className="mt-2 col-span-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                    : {request.sizeOfLand}
+                  </p>
+                </div>
+                <div className="mt-3 md:mt-2 grid grid-cols-5">
+                  <p className="flex col-span-1 items-center text-sm text-gray-500">
+                    Added date
+                  </p>
+                  <p className="md:mt-0 col-span-2 flex items-center text-sm text-gray-500">
+                    : {moment(request.createdAt).format("MMMM Do YYYY")}{" "}
+                    <br></br>
+                    &nbsp; {moment(request.createdAt).format("LTS")}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 ">
+                  <div className="col-start-4 col-span-1 justify-end flex">
+                    <button
+                      className="flex min-w-fit bg-red-500 text-white py-1 px-4 rounded-lg hover:bg-red-600 transition-colors"
+                      onClick={() => onDelete(request._id)}
+                    >
+                      <RiDeleteBin6Line
+                        className="mt-0 mr-0 md:mt-1 md:mr-1"
+                        size={18}
+                      />
+                      <p className="hidden md:block">Delete</p>
+                    </button>
                   </div>
-                  <div className="mt-2 grid grid-cols-5 ">
-                    <p className="flex col-span-1 items-center text-sm text-gray-500 ">
-                      Type
-                    </p>
-                    <p className="mt-2 col-span-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                      : {request.type}
-                    </p>
-                  </div>
-                  <div className="mt-2 grid grid-cols-5">
-                    <p className="flex col-span-1 items-center text-sm text-gray-500">
-                      Hectares
-                    </p>
-                    <p className="mt-2 col-span-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                      : {request.sizeOfLand}
-                    </p>
-                  </div>
-                  <div className="mt-3 md:mt-2 grid grid-cols-5">
-                    <p className="flex col-span-1 items-center text-sm text-gray-500">
-                      Added date
-                    </p>
-                    <p className="md:mt-0 col-span-2 flex items-center text-sm text-gray-500">
-                      : {moment(request.createdAt).format("MMMM Do YYYY")}{" "}
-                      <br></br>
-                      &nbsp; {moment(request.createdAt).format("LTS")}
-                    </p>
-                  </div>
-                  <div className="grid grid-cols-5 ">
-                    <div className="col-start-4 col-span-1 justify-end flex">
-                      <button
-                        className="flex min-w-fit bg-red-500 text-white py-1 px-4 rounded-lg hover:bg-red-600 transition-colors"
-                        onClick={() => onDelete(request._id)}
-                      >
-                        <RiDeleteBin6Line
-                          className="mt-0 mr-0 md:mt-1 md:mr-1"
-                          size={18}
-                        />
-                        <p className="hidden md:block">Delete</p>
-                      </button>
-                    </div>
-                    <div className="col-span-1 justify-center flex">
-                      <button
-                        className="flex w-fit text-white bg-green-500 py-1 px-4 rounded-lg hover:bg-green-600 transition-colors"
-                        onClick={() => onUpdate(request._id)}
-                      >
-                        <AiOutlineEdit
-                          className="mt-0 mr-0 md:mt-1 md:mr-1"
-                          size={18}
-                        />
-                        <p className="hidden md:block"> Update</p>
-                      </button>
-                    </div>
+                  <div className="col-span-1 justify-center flex">
+                    <button
+                      className="flex w-fit text-white bg-green-500 py-1 px-4 rounded-lg hover:bg-green-600 transition-colors"
+                      onClick={() => onUpdate(request._id)}
+                    >
+                      <AiOutlineEdit
+                        className="mt-0 mr-0 md:mt-1 md:mr-1"
+                        size={18}
+                      />
+                      <p className="hidden md:block"> Update</p>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
+          </div>
+        ))}
 
-        {isSearchResultExists === false && (
+        {isLoading === true ? (
           <>
-            <div>No results found</div>
+            <div>
+              <div className="flex justify-center mt-24">
+                <CircularProgress color="success" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            {isSearchResultExists === false && (
+              <>
+                <div className="grid justify-center mt-16">
+                  <div className="flex justify-center">
+                    <FiAlertCircle size={80} color="#63736b" />
+                  </div>
+                  <div className="font-semibold text-gray-500 text-xl mt-4">
+                    No results found
+                  </div>
+                </div>
+              </>
+            )}
           </>
         )}
       </MyRequestsWrapper>
