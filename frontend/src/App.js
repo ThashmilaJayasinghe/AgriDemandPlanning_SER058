03a5e87763825ef99c766af70bbfdf86fafa407c
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-09-10 22:47:24 +0530

    buyer route changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
-import Login from './pages/Login';
-import AdminLayout from './components/layouts/Admin';
-import FarmerLayout from './components/layouts/Farmer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminLayout from "./components/layouts/Admin";
+import FarmerLayout from "./components/layouts/Farmer";
+import BuyerLayout from "./components/layouts/Buyer";
 
 function App() {
   return (
-    
     <div>
       <Router>
         <div>
           <Routes>
-              <Route path='/' element={<Login />} />
-              <Route path='/admin/*' element={<AdminLayout />} />
-              <Route path='/farmer/*' element={<FarmerLayout />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/admin/*" element={<AdminLayout />} />
+            <Route path="/farmer/*" element={<FarmerLayout />} />
+            <Route path="/buyer/*" element={<BuyerLayout />} />
           </Routes>
         </div>
       </Router>
