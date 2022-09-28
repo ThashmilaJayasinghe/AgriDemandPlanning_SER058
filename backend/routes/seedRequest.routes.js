a4c25a76f85ab5eff21e78a5131648717d6fbb55
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-09-29 01:49:28 +0530

    edit request route added

--- backend/routes/seedRequest.routes.js
+++ backend/routes/seedRequest.routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const {createSeedRequest, viewFarmerSeedRequests, deleteSeedRequest} = require("../api/seedRequest.api");
+const {
+  createSeedRequest,
+  viewFarmerSeedRequests,
+  deleteSeedRequest,
+  updateSeedRequest,
+} = require("../api/seedRequest.api");
 const router = express.Router();
 
 router.post("/create-seed-request", createSeedRequest);
-router.get('/view-seed-request', viewFarmerSeedRequests)
-// router.put("/update-seed-request");
+router.get("/view-seed-request", viewFarmerSeedRequests);
+router.put("/update-seed-request", updateSeedRequest);
 router.delete("/delete-seed-request", deleteSeedRequest);
 
 module.exports = router;
